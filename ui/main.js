d83b2a87df2ceac4d245db04c04abd03cffec3d2
badsha141975 <badsha141975@gmail.com>
2016-10-06 19:38:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,12 @@
 //Code of Counter
-var button=ducument.getElementById('counter'); // Select our button
+var button = ducument.getElementById('counter'); // Select our button
 
 //When the button is clicked, let us attach a function to it which should 
 //first make a request to the counter end pt, capture response and store it in a var.
 //The var should be rendered in the correct span. Let's do this(pt 5) first
-var counter=0;
-button.onclick=function() {
-counter=counter+1;
+var counter = 0;
+button.onclick = function() {
+counter = counter+1;
 var span=document.getElementbyId('count');
-span.innerHTML=counter.toString();
+span.innerHTML = counter.toString();
 };
